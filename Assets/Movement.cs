117bfa7e0ee1a020c3f4dabcf5d2cb45e7099221
spingospingo <wobblyterror@gmail.com>
2020-02-25 21:33:03 -0800

    initial upload

---
+++ Assets/Movement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+    public LayerMask navigable; //mesh that can be clicked on for movement
+    public Camera overheadCam;
+    public Camera playerCam;
+
+    private NavMeshAgent playerAgent;
+    private Ray clickRay;
+    private Camera cam;
+
+    void Start()
+    {
+        playerAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void OnGUI()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if(overheadCam.enabled) //seems to run twice per click?
+            {
+                cam = overheadCam;
+            }
+            else if (playerCam.enabled)
+            {
+                cam = playerCam;
+            }
+
+            //shoots ray from camera towards mouseclick
+            clickRay = cam.ScreenPointToRay(Input.mousePosition);
+            //point of collision from camera raycast
+            RaycastHit hitInfo; 
+
+            //if ray collides with navmesh...
+            if (Physics.Raycast (clickRay, out hitInfo, 100, navigable))
+            {
+                //...tell player to move to that point
+                playerAgent.SetDestination(hitInfo.point);
+            }
+        }
+    }
+}
