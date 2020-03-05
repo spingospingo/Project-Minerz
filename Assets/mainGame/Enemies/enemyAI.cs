33fc1c6e705785126df7a30d3291d236f10beb1a
spingospingo <wobblyterror@gmail.com>
2020-03-04 21:48:14 -0800

    added enemy object
    enemies follow player and have health variable

---
+++ Assets/mainGame/Enemies/enemyAI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyAI : MonoBehaviour
+{
+    private UnityEngine.AI.NavMeshAgent enemy;
+    private GameObject player;
+    private Vector3 target;
+    private int health = 100; //placeholder value
+
+    void Start()
+    {
+        enemy = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        player = GameObject.Find("Player");
+    }
+
+    void FixedUpdate()
+    {
+        target = new Vector3(player.transform.position.x,
+            player.transform.position.y,
+            player.transform.position.z);
+        enemy.SetDestination(target);
+
+        if (health <= 0)
+        {
+            Destroy(gameObject, 1);
+            Debug.Log("blargh im dead");
+        }
+    }
+}
