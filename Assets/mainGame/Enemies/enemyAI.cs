775fb87099c425a088a2a25445fae6d6e5606b56
spingospingo <wobblyterror@gmail.com>
2020-03-05 00:10:23 -0800

    created attributes component

--- Assets/mainGame/Enemies/enemyAI.cs
+++ Assets/mainGame/Enemies/enemyAI.cs
@@ -1,31 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class enemyAI : MonoBehaviour
 {
-    private UnityEngine.AI.NavMeshAgent enemy;
+    private NavMeshAgent enemyAgent;
     private GameObject player;
     private Vector3 target;
-    private int health = 100; //placeholder value
+    private NavMeshPath path;
 
     void Start()
     {
-        enemy = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        enemyAgent = GetComponent<NavMeshAgent>();
+        //NOTE: change player once multiple players exist
         player = GameObject.Find("Player");
+        path = new NavMeshPath();
     }
 
     void FixedUpdate()
     {
+        focusTarget();
+    }
+
+    private void focusTarget()
+    {
+        //convert player location to Vector3
         target = new Vector3(player.transform.position.x,
             player.transform.position.y,
             player.transform.position.z);
-        enemy.SetDestination(target);
-
-        if (health <= 0)
+        //create path towards player
+        enemyAgent.CalculatePath(target, path);
+        //if enemy can path to player...
+        if (path.status == NavMeshPathStatus.PathComplete)
         {
-            Destroy(gameObject, 1);
-            Debug.Log("blargh im dead");
+            //...go to player
+            enemyAgent.SetDestination(target);
         }
     }
 }
