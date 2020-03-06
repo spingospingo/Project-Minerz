cd80fee7b8d44613c2cb2d8747276130d29287db
DrSpaceMeme <61670003+DrSpaceMeme@users.noreply.github.com>
2020-03-06 16:39:20 -0600

    Player GUI Resoltuion Fixes

---
+++ Assets/mainGame/Minerals/mineralAttributes.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mineralAttributes : MonoBehaviour
+{
+    private Material mineralType;
+
+    private int mineralAmount;
+    public int MineralAmount  
+    {
+        get { return mineralAmount; }
+        set
+        {
+
+        }
+
+    }
+
+
+    void Awake()
+    {
+        mineralAmountScript();
+        mineralTypeScript(); 
+    }
+
+    void Update()
+    {
+
+    }
+
+
+        private void mineralAmountScript()
+        {
+            mineralAmount = Random.Range(400, 500);
+        }
+
+
+        private void mineralTypeScript()
+        {
+            mineralType = GetComponent<Renderer>().material;
+            Debug.Log(mineralType);
+        } 
+    
+        private void mineralAmountUpdateScript() //This Method will be used to "mine" from the game object, for now it is used to test the mineral amount during runtime
+        {
+
+            bool testbool = true;
+
+                if (testbool == true)
+                {
+                    mineralAmount--;
+                }
+                
+                if (testbool == false)
+                {
+                    mineralAmount++;
+                }
+
+                if (mineralAmount > 499)
+                 {
+                    testbool = true;
+                 }
+
+        }
+
+
+    }
