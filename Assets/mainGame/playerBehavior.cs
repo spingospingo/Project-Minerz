1c52389468ed053dc6b06ba909b7517c6f48ef1e
spingospingo <wobblyterror@gmail.com>
2020-03-09 22:47:19 -0700

    added mining capability
    userControls script now controls the player's actions, and the player can now slowly mine each resource 1 at a time. GUI also correctly displays player's inventory.

--- Assets/mainGame/playerBehavior.cs
+++ Assets/mainGame/playerBehavior.cs
@@ -11,60 +11,94 @@ public class playerBehavior : MonoBehaviour
     private Ray clickRay;
     private Camera cam;
 
-    private GameObject selectedObject;
-    public GameObject SelectedObject
+    private int mat1Inv, mat2Inv, mat3Inv, mat4Inv, mat5Inv;
+    public int Mat1Inv
     {
-        get { return selectedObject; }
-        set { }
+        get { return mat1Inv; }
+        set { mat1Inv = value; }
     }
+
+    public int Mat2Inv
+    {
+        get { return mat2Inv; }
+        set { mat2Inv = value; }
+    }
+
+    public int Mat3Inv
+    {
+        get { return mat3Inv; }
+        set { mat3Inv = value; }
+    }
+
+    public int Mat4Inv
+    {
+        get { return mat4Inv; }
+        set { mat4Inv = value; }
+    }
+
+    public int Mat5Inv
+    {
+        get { return mat5Inv; }
+        set { mat5Inv = value; }
+    }
+
+    private enum State
+    {
+        Idle,
+        Moving,
+        Mining,
+    }
+    private State state;
+
+    private bool isMining;
+    private bool isChecking;
+
+    private GameObject objectToInteractWith;
 
     void Start()
     {
-        selectedObject = GameObject.Find("Floor"); //instantiate to "no selection"
-        cam = Camera.main;
         playerAgent = GetComponent<NavMeshAgent>();
         navigable = LayerMask.GetMask("Floor");
         interactable = LayerMask.GetMask("Interactable");
+        state = State.Idle;
     }
 
-    void OnGUI()
+    void Update()
     {
-        if (Input.GetMouseButtonDown(1)) //right click
+        switch (state)
         {
-            movePlayer();
-        }
-        else if (Input.GetMouseButtonDown(0)) //left click
-        {
-            selectObject();
+            case State.Idle:
+
+                break;
+            case State.Moving:
+
+                break;
+            case State.Mining:
+                if (!isMining && playerAgent.remainingDistance <= 1.5f
+                    && playerAgent.remainingDistance != 0)
+                {
+                    StartCoroutine(mine(1));
+                }
+                break;
         }
     }
 
-    private void movePlayer()
+    public void controlPlayer(RaycastHit hitInfo)
     {
-        //instantiate path variable to check if 
         NavMeshPath path = new NavMeshPath();
 
-        //shoots ray from camera towards mouseclick
-        clickRay = cam.ScreenPointToRay(Input.mousePosition);
-        //point of collision from camera raycast
-        RaycastHit hitInfo;
-
         //if ray collides with an interactable object...
-        if (Physics.Raycast(clickRay, out hitInfo, 150, interactable))
+        if (hitInfo.collider.gameObject.tag == "Mineral"
+            || hitInfo.collider.gameObject.tag == "Rock")
         {
-            //Set object to interactableGameObject and get object attributes
-            selectedObject = hitInfo.collider.gameObject;
+            objectToInteractWith = hitInfo.collider.gameObject;
 
-            playerAgent.CalculatePath(hitInfo.point, path);
-            //if player can move to the point...
-            if (path.status == NavMeshPathStatus.PathComplete)
-            {
-                //...tell player to move to that point
-                playerAgent.SetDestination(hitInfo.point);
-            }
+            playerAgent.SetDestination(hitInfo.point);
+            state = State.Mining;
+            playerAgent.stoppingDistance = 1.5f;
         }
         //if ray collides with navmesh...
-        else if (Physics.Raycast(clickRay, out hitInfo, 150, navigable))
+        else
         {
             playerAgent.CalculatePath(hitInfo.point, path);
             //if player can move to the point...
@@ -72,20 +106,35 @@ public class playerBehavior : MonoBehaviour
             {
                 //...tell player to move to that point
                 playerAgent.SetDestination(hitInfo.point);
+                state = State.Moving;
+                playerAgent.stoppingDistance = 0;
             }
         }
     }
 
-    private void selectObject()
+    private IEnumerator mine(int amount)
     {
-        clickRay = cam.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hitInfo;
-
-        //if ray collides with object
-        if (Physics.Raycast(clickRay, out hitInfo, Mathf.Infinity))
+        isMining = true;
+        switch(objectToInteractWith.name)
         {
-            selectedObject = hitInfo.collider.gameObject;
+            case "material1(Clone)":
+                mat1Inv += objectToInteractWith.GetComponent<mineralAttributes>().mine(amount);
+                break;
+            case "material2(Clone)":
+                mat2Inv += objectToInteractWith.GetComponent<mineralAttributes>().mine(amount);
+                break;
+            case "material3(Clone)":
+                mat3Inv += objectToInteractWith.GetComponent<mineralAttributes>().mine(amount);
+                break;
+            case "material4(Clone)":
+                mat4Inv += objectToInteractWith.GetComponent<mineralAttributes>().mine(amount);
+                break;
+            case "material5(Clone)":
+                mat5Inv += objectToInteractWith.GetComponent<mineralAttributes>().mine(amount);
+                break;
         }
+        yield return new WaitForSeconds(1);
+        isMining = false;
     }
 }
 
---
+++ Assets/mainGame/userControls.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class userControls : MonoBehaviour
+{
+    private LayerMask navigable, interactable;
+
+    private Ray clickRay;
+    private Camera cam;
+    private playerBehavior player;
+
+    private GameObject selectedObject;
+    public GameObject SelectedObject
+    {
+        get { return selectedObject; }
+        set { }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectedObject = GameObject.Find("Floor"); //instantiate to "no selection"
+        navigable = LayerMask.GetMask("Floor");
+        interactable = LayerMask.GetMask("Interactable");
+        cam = Camera.main;
+        player = GameObject.Find("Player").GetComponent<playerBehavior>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1)) //right click
+        {
+            clickRay = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            //if ray collides with an interactable object...
+            if (Physics.Raycast(clickRay, out hitInfo, Mathf.Infinity, navigable | interactable))
+            {
+                player.controlPlayer(hitInfo);
+            }
+        }
+        else if (Input.GetMouseButtonDown(0)) //left click
+        {
+            selectObject();
+        }
+    }
+
+    private void selectObject()
+    {
+        clickRay = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        //if ray collides with ground OR object
+        if (Physics.Raycast(clickRay, out hitInfo, Mathf.Infinity, navigable | interactable))
+        {
+            selectedObject = hitInfo.collider.gameObject;
+        }
+    }
+}
